0f8219b1b963b2d21c36ca392861fc1c3843ac07
AknietB <barahanakniet@gmail.com>
2025-03-02 16:22:54 +0500

    Delete func12.py

--- func12.py
+++
@@ -1,9 +0,0 @@
-def histogram():
-    a = int(input())
-    arr=[]
-    for i in range(a):
-        elem = int(input())
-        arr.append(elem)
-    for i in range(len(arr)):
-        print("*" * arr[i])
-histogram()
